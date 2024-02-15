3c41894857aa0fc898e6179d085174fb85f32f60
CPerelson <121648303+CPerelson@users.noreply.github.com>
2024-02-15 17:45:04 -0500

    Update BinarySearchTree.java

--- BSTHW/BinarySearchTree.java
+++ BSTHW/BinarySearchTree.java
@@ -2,78 +2,81 @@ package BST;
 
 import java.util.*;
 
-public class BinarySearchTree<T extends Comparable<T>> implements BSTInterface<T>{
+public class BinarySearchTree<T extends Comparable<T>> implements BSTInterface<T> {
 
     private BSTNode<T> root;
     private int counter = 0;
 
-    public void add(T data){
-        if (root == null){
+    public void add(T data) {
+        if (root == null) {
             root = new BSTNode<>(data);
-        }else {
+        } else {
             addHelper(data, root);
         }
     }
+
     private void addHelper(T data, BSTNode<T> node) {
         int compare = data.compareTo(node.getData());
-        if(compare > 0) {
-	        if (node.getRight() == null){
-	            node.setRight(new BSTNode<>(data));
-	        }else {
-	            addHelper(data, node.getRight());
-	        }
-        }else {
-        	if (node.getLeft() == null){
-	            node.setLeft(new BSTNode<>(data));
-	        }else {
-	            addHelper(data, node.getLeft());
-	        }
+        if (compare > 0) {
+            if (node.getRight() == null) {
+                node.setRight(new BSTNode<>(data));
+            } else {
+                addHelper(data, node.getRight());
+            }
+        } else {
+            if (node.getLeft() == null) {
+                node.setLeft(new BSTNode<>(data));
+            } else {
+                addHelper(data, node.getLeft());
+            }
         }
     }
 
-    public boolean contains(T target){
+    public boolean contains(T target) {
         System.out.println("check if the binary search tree contains the target element");
         return containsHelper(target, root);
     }
 
-    private boolean containsHelper(T target, BSTNode<T> node){
+    private boolean containsHelper(T target, BSTNode<T> node) {
         System.out.println("check if the subtree contains the target element");
-        if (node == null){
+        if (node == null) {
             System.out.println("target element is not found in the subtree");
             return false;
         }
         int compare = target.compareTo(node.getData());
-        if (compare < 0){
+        if (compare < 0) {
             System.out.println("Target element is smaller than the current node, searching in the left subtree");
             return containsHelper(target, node.getLeft());
         } else if (compare > 0) {
             System.out.println("Target element is larger than the current node, searching in the right subtree");
             return containsHelper(target, node.getRight());
-        }else {
+        } else {
             System.out.println("Target element found in subtree");
             return true;
         }
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         System.out.println("check if BST is empty");
-        if (root == null){
+        if (root == null) {
             return true;
-        }else {
+        } else {
             return false;
         }
     }
-    public T min(){
+
+    public T min() {
         System.out.println("find the minimum element ");
-        if (isEmpty()){
+        if (isEmpty()) {
             return null;
         }
         BSTNode<T> node = root;
-        while (node.getLeft() != null){
+        while (node.getLeft() != null) {
             node = node.getLeft();
         }
         return node.getInfo();
     }
+
     public T max() {
         System.out.println("Finding the maximum element");
         if (isEmpty()) {
@@ -92,11 +95,11 @@ public class BinarySearchTree<T extends Comparable<T>> implements BSTInterface<T
         return counter;
     }
 
-    public Iterator<T> getIterator(BSTInterface.Traversal orderType){
+    public Iterator<T> getIterator(BSTInterface.Traversal orderType) {
         System.out.println("Creating an iterator for traversing the binar search tree");
 
         final LinkedQueue<T> infoQueue = new LinkedQueue();
-        if (orderType == Traversal.Preorder){
+        if (orderType == Traversal.Preorder) {
             System.out.println("Traversing the tree in pre-order traversal to populate the iterator queue");
             preOrder(root, infoQueue);
         } else if (orderType == Traversal.Inorder) {
@@ -117,35 +120,35 @@ public class BinarySearchTree<T extends Comparable<T>> implements BSTInterface<T
             @Override
             public T next() {
                 System.out.println("get the next element from the iterator queue");
-                if (!hasNext()){
+                if (!hasNext()) {
                     throw new IndexOutOfBoundsException("Illegal invocation of next in binary search tree");
-                }               
+                }
                 return infoQueue.dequeue();
             }
 
-            public void remove(){
+            public void remove() {
                 System.out.println("Unsupported remove attemped on binary search tree iterator");
                 throw new UnsupportedOperationException("Unsupported remove attempted on binary search tree iterator");
             }
         };
     }
-    
+
     private void inOrder(BSTNode<T> node, LinkedQueue<T> queue) {
-        if(node == null) 
-        	return;
+        if (node == null)
+            return;
 
         // Visit current node
         System.out.println("Visiting node: " + node.getData());
         // Traverse left subtree
         inOrder(node.getLeft(), queue);
         queue.enqueue(node.getData());
-     // Traverse right subtree
+        // Traverse right subtree
         inOrder(node.getRight(), queue);
     }
 
     // Pre-order traversal
     private void preOrder(BSTNode<T> node, LinkedQueue<T> queue) {
-        if(node == null) {
+        if (node == null) {
             return;
         }
 
@@ -162,7 +165,7 @@ public class BinarySearchTree<T extends Comparable<T>> implements BSTInterface<T
 
     // Post-order traversal
     private void postOrder(BSTNode<T> node, LinkedQueue<T> queue) {
-        if(node == null) {
+        if (node == null) {
             return;
         }
 
@@ -177,25 +180,61 @@ public class BinarySearchTree<T extends Comparable<T>> implements BSTInterface<T
         queue.enqueue(node.getData());
     }
 
-    public String toString() {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Binary search tree: \n");
-        if (root != null) {
-            Queue<BSTNode<T>> queue = new LinkedList<>();
-            queue.add(root);
-            while (!queue.isEmpty()){
-                BSTNode<T> node = queue.poll();
-                stringBuilder.append(node.getInfo()).append(" ");
-                if (node.getLeft() != null){
-                    queue.add(node.getLeft());
+    public void breadthFirstSearch() {
+        BSTNode<T> node = null;
+        if (node != null) {
+            LinkedQueue<BSTNode<T>> queue = new LinkedQueue<>();
+            queue.enqueue(node);
+            while (!queue.isEmpty()) {
+                node = queue.dequeue();
+                System.out.println(node.getInfo());
+                if (node.getLeft() != null) {
+                    queue.enqueue(node.getLeft());
                 }
-                if (node.getRight() != null){
-                    queue.add(node.getRight());
+                if (node.getRight() != null) {
+                    queue.enqueue(node.getRight());
                 }
             }
         }
-        return stringBuilder.toString();
     }
 
+    public String depthFirstSearch() {
+        BSTNode<T> node;
+        if (node != null) {
+            Stack<BSTNode<T>> stack = new Stack<>();
+            stack.push(node);
+            while (!stack.isEmpty()) {
+                BSTNode<T> node = stack.pop();
+                System.out.println(node.getInfo());
+                if (node.getRight() != null) {
+                    stack.push(node.getRight());
+                }
+                if (node.getLeft() != null) {
+                    stack.push(node.getLeft());
+                }
+            }
+        }
+
+        public String toString () {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("Binary search tree: \n");
+            if (root != null) {
+                Queue<BSTNode<T>> queue = new LinkedList<>();
+                queue.add(root);
+                while (!queue.isEmpty()) {
+                    BSTNode<T> node = queue.poll();
+                    stringBuilder.append(node.getInfo()).append(" ");
+                    if (node.getLeft() != null) {
+                        queue.add(node.getLeft());
+                    }
+                    if (node.getRight() != null) {
+                        queue.add(node.getRight());
+                    }
+                }
+            }
+            return stringBuilder.toString();
+        }
+
+    }
 }
 
