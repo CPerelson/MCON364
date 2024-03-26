3753f996ee93d61bc948d63b9a0df3ffbe2dcbcf
CPerelson <121648303+CPerelson@users.noreply.github.com>
2024-03-25 23:58:13 -0400

    Update MapEntry.java

--- HashMap/MapEntry.java
+++ HashMap/MapEntry.java
@@ -2,10 +2,15 @@ package Maps;
 
 public class MapEntry<K, V>
 {
+    //MapEntry class is generic types.
+    //has 3 instances key - keu of the entry, value - value associated with the key,
+    //next - a reference to the next MapEntry in the linkedlist used for handling collisions
     protected K key;
     protected V value;
     MapEntry<K, V> next;
 
+    //MapEntry constructor initialized the key and value with the provided arguments and sets next
+    //to null - which indicates that it's the end of the linkedlist
     MapEntry(K key, V value)
     {
         this.key = key;
@@ -13,11 +18,12 @@ public class MapEntry<K, V>
         this.next = null;
     }
 
+    //getters and setters for the key and value instance variables
     public K getKey() {return key;}
     public V getValue(){return value;}
     public void setValue(V value){
-        this.value = value;
     }
+
     @Override
     public String toString()
     // Returns a string representing this MapEntry.
@@ -26,4 +32,3 @@ public class MapEntry<K, V>
     }
 
 }
-
