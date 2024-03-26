2ea6b78c1bc9fecef4a0cf8a1928536ba844255e
CPerelson <121648303+CPerelson@users.noreply.github.com>
2024-03-26 16:09:38 -0400

    Update MainGraph.java

--- Graphs/MainGraph.java
+++ Graphs/MainGraph.java
@@ -3,7 +3,8 @@ package Graphs;
 public class MainGraph {
     public static void main(String[] args){
         //creating a social network graph
-        Graph socialNetwork = new Graph();
+        Graph socialNetwork = new Graph();//create a new graph object to represent the social network
+        //add individuals to the social network
         socialNetwork.addIndividual("Leah");
         socialNetwork.addIndividual("David");
         socialNetwork.addIndividual("John");
@@ -11,7 +12,7 @@ public class MainGraph {
         socialNetwork.addIndividual("Devorah");
         socialNetwork.addIndividual("Sara");
         socialNetwork.addIndividual("Henry");
-
+        //add friendships between individuals
         socialNetwork.addFriendship("Leah", "David");
         socialNetwork.addFriendship("Leah", "Alex");
         socialNetwork.addFriendship("David", "Henry");
@@ -27,7 +28,7 @@ public class MainGraph {
         System.out.println("Social network adjacency list:");
         socialNetwork.printAdjacencyList();
 
-        //finding paths between cities
+        //finding paths between individuals
         System.out.println("\nAre David and Devorah connected? "+socialNetwork.areConnected("David", "Devorah"));
         System.out.println("Are Alex and Sara connected? "+socialNetwork.areConnected("Alex", "Sara"));
         System.out.println("Are John and Leah connected? "+socialNetwork.areConnected("John", "Leah"));
